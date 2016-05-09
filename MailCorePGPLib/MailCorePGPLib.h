1fea398268a439158c54385bfbb9a21c138787ba
Adam Cmiel <adamcmiel@gmail.com>
2016-05-09 16:55:24 -0500

    Initial Commit

---
+++ MailCorePGPLib/MailCorePGPLib.h
@@ -0,0 +1,19 @@
+//
+//  MailCorePGPLib.h
+//  MailCorePGPLib
+//
+//  Created by Adam Cmiel on 5/9/16.
+//  Copyright © 2016 Adam Cmiel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MailCorePGPLib.
+FOUNDATION_EXPORT double MailCorePGPLibVersionNumber;
+
+//! Project version string for MailCorePGPLib.
+FOUNDATION_EXPORT const unsigned char MailCorePGPLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MailCorePGPLib/PublicHeader.h>
+
+
